229ea9af72b21578f1b343697ac088c7aaf1e184
alienor darenberg <alienordarenberg@gmail.com>
2023-03-07 15:45:04 -0600

    hurv

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'articles#index'
-  get 'articles', to: 'articles#index'
-  get 'articles/new', to: 'articles#new', as: :new_article
-  get 'articles/:id', to: 'articles#show', as: :article
-  post 'articles', to: 'articles#create'
-  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
-  patch 'articles/:id', to: 'articles#update'
-  delete 'articles/:id', to: 'articles#destroy'
+resources :articles
   # Defines the root path route ('/')
   # root 'articles#index'
 end
